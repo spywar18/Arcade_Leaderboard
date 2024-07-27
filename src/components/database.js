8f3d6e68de820d76c933a24debfd928236ed2867
Shikhar Singh <singh.shikhar0712@gmail.com>
2024-07-28 04:02:13 +0530

    Initial commit

--- src/components/database.js
+++ src/components/database.js
@@ -1,44 +1,36 @@
 export const Leaderboard = [
     {
-        name: "Shawn Hanna",
-        location: "India",
-        score : 1550,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name: "Manish",
+        location: "Kumar",
+        score : 22,
         dt: "2022-02-10"
     },
     {
-        name: "Fidel Hand",
-        location: "USA",
-        score : 2310,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-01-01"
+        name: "Sujal",
+        location: "Gupta",
+        score : 30,
     },
     {
-        name: "Bessie Hickle",
-        location: "Chaina",
-        score : 350,
-        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-08-17"
+        name: "Krishi",
+        location: "Shah",
+        score : 20,
     },
     {
-        name: "Adella Wunsch",
-        location: "Japan",
-        score : 2100,
-        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name: "Saksham",
+        location: "Singh",
+        score : 26,
         dt: "2021-10-23"
     },
     {
-        name: "Clair O'Connell",
-        location: "London",
-        score : 1250,
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name: "Bilal",
+        location: "Ahmad",
+        score : 21,
         dt: "2022-01-22"
     },
     {
-        name: "Kameron Prosacco",
-        location: "Canada",
-        score : 5250,
-        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        name: "Ajay ",
+        location: "Berwal",
+        score : 34,
         dt: "2022-01-21"
     }
 ]
